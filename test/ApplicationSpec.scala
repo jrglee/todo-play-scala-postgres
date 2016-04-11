f135dc4342f6370726071a80b19cf2a6914fd668
Jorge Lee <jrg.lee@gmail.com>
2016-04-11 00:51:49 -0700

    Remove generated code. Test with in-memory db

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,33 +1,30 @@
+import org.scalatest.TestData
 import org.scalatestplus.play._
-import play.api.test._
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers._
+import play.api.test._
 
-import scala.util.parsing.json.JSONArray
-
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 class ApplicationSpec extends PlaySpec with OneAppPerTest {
+
+  override def newAppForTest(td: TestData) = new GuiceApplicationBuilder()
+    .configure(inMemoryDatabase())
+    .build()
 
   "Routes" should {
 
-    "send 404 on a bad request" in  {
-      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+    "send 404 on a bad request" in {
+      route(app, FakeRequest(GET, "/boum")).map(status) mustBe Some(NOT_FOUND)
     }
-
   }
 
   "TodoController" should {
 
     "return empty content" in {
-      val home = route(app, FakeRequest(GET, "/")).get
+      val home = route(app, FakeRequest(GET, "/todo")).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("application/json")
       contentAsJson(home).as[List[String]] mustBe empty
     }
   }
-
 }
